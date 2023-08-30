af9098ff89d0a09c0a942fceea94062ab6764ec2
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-08-30 14:06:21 +0700

    Create Soal6.py

---
+++ Soal6.py
@@ -0,0 +1,22 @@
+# 6. Tentukan algoritma dan pseudocode volume kerucut
+#    (diameter = 5, tinggi = 4) 
+
+# Tentukan nilai jari jari
+# Tentukan nilai tinggi
+# Lakukan proses perhitungan volume kerucut: V = 1/3 * phi * (r^2) * t
+# Berikan output hasil volume kerucut
+
+# MULAI
+# INPUT NILAI JARI-JARI / TENTUKAN NILAI JARI JARI DARI DIAMETER
+# INPUT NILAI TINGGI
+# LAKUKAN PROSES PERHITUNGAN VOLUME KERUCUT: V = 1/3 * phi * (r^2) * t
+# BERIKAN OUTPUT HASIL VOLUME KERUCUT
+# SELESAI
+
+diameter = 5
+r = diameter / 2
+t = 4
+
+volume_kerucut = 1/3 * 3.14 * (r**2) * t
+
+print("Volume kerucut adalah: ", volume_kerucut)
