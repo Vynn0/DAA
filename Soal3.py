24f514617e455bcf1eafb7d24661949b524ebbe9
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-08-30 14:04:53 +0700

    Create Soal3.py

---
+++ Soal3.py
@@ -0,0 +1,13 @@
+"""
+3. Diketahui sebuah segitiga memiliki ukuran sebagai berikut:
+Alas 25
+Tinggi 30
+Hitung luas dari segitiga tersebut.
+"""
+
+alas = 25
+tinggi = 30
+
+luas = 0.5 * alas * tinggi
+
+print(luas)
