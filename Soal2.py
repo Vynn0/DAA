fd68b134930c5a531de32bcb21537ff547454f68
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-08-30 16:19:07 +0700

    Update Soal2.py

--- Soal2.py
+++ Soal2.py
@@ -1,14 +1,58 @@
 # 2. Fungsi untuk menukar posisi dua variabel x dan y, dengan kasus : 
 # Ada 2 buah: manggis dan pisang. Manggis di piring 1, Pisang di piring 2. Piring 3 kosong.
 
-piring1 = "Manggis"
-piring2 = "Pisang"
-piring3 = None
+"""
+Algoritma
 
-temp = piring1
-piring1 = piring2
-piring2 = temp
+Definisikan tukar_posisi(x,y)
+Deklarasikan piring dengan buah nya yang sudah ditentukan
+Cetak piring sebelum pertukaran
 
-print("Piring 1 berisi: ", piring1)
-print("Piring 2 berisi: ", piring2)
-print("Piring 3 berisi: ", piring3)
+Tukar posisi piring 1 dan 2 mengunnakan fungsi tukar_posisi(x,y)
+Cetak piring sesudah pertukaran
+"""
+
+"""
+Pseudocode
+
+DEFINISIKAN TUKAR_POSISI(x,y) sebagai:
+  temp = x
+  x = y
+  y = temp
+  return x,y
+
+DEKLARASIKAN 
+manggis = "piring 1"
+pisang = "piring 2"
+
+CETAK PIRING SEBELUM PERTUKARAN
+print("Sebelum pertukaran:")
+print("Manggis:", manggis)
+print("Pisang:", pisang)
+
+tukar_posisi manggis dengan pisang
+
+CETAK PIRING SETELAH PERTUKARAN
+print("\nSetelah pertukaran:")
+print("Manggis:", manggis)
+print("Pisang:", pisang)
+"""
+
+def tukar_posisi(x, y):
+    temp = x
+    x = y
+    y = temp
+    return x, y
+
+manggis = "piring 1"
+pisang = "piring 2"
+
+print("Sebelum pertukaran:")
+print("Manggis:", manggis)
+print("Pisang:", pisang)
+
+manggis, pisang = tukar_posisi(manggis, pisang)
+
+print("\nSetelah pertukaran:")
+print("Manggis:", manggis)
+print("Pisang:", pisang)
