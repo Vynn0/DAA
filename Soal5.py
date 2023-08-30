c297b190dc8dc7fe522100aad16d0532ac169a4a
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-08-30 14:06:06 +0700

    Create Soal5.py

---
+++ Soal5.py
@@ -0,0 +1,18 @@
+# 5. Tentukan algoritma dan pseudocode volume tabung (jari-jari = 3, tinggi = 5) 
+
+# Masukkan nilai jari-jari
+# Masukkan nilai tinggi
+# Volume = phi * (r^2) * t
+
+# BEGIN
+# INPUT NILAI JARI-JARI
+# INPUT NILAI TINGGI
+# V = phi * (r^2) * t
+# Print output volume
+# SELESAI
+
+r = 3
+t = 5
+V = 3.14 * (3**2) * 5
+
+print("Volume tabung:", V)
