81ac878cce5248019f34af1565b97d8decf14b29
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-08-30 14:04:03 +0700

    Create Soal2.py

---
+++ Soal2.py
@@ -0,0 +1,13 @@
+# 2. Fungsi untuk menukar posisi dua variabel x dan y, dengan kasus : 
+# Ada 2 buah: manggis dan pisang. Manggis di piring 1, Pisang di piring 2. Piring 3 kosong.
+
+piring1 = "Manggis"
+piring2 = "Pisang"
+piring3 = None
+
+temp = piring1
+piring1 = piring2
+piring2 = temp
+
+print("Piring 1 berisikan: ", piring1)
+print("Piring 2 berisikan: ", piring2)
