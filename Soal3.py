a07af9a054bf04768dd1569cb2ca5a408c89ffd7
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-08-31 10:48:11 +0700

    Update Soal3.py

--- Soal3.py
+++ Soal3.py
@@ -5,6 +5,25 @@ Tinggi 30
 Hitung luas dari segitiga tersebut.
 """
 
+"""
+ALGORITMA
+
+1. Tentukan alas dan tinggi
+2. Cari luas segitiga menggunakan rumus 1/2 * alas * tinggi
+3. Cetak hasil luas
+"""
+
+"""
+PSEUDOCODE
+
+1. Deklarasikan alas dan tinggi sebagai variabel
+alas = 25
+tinggi = 30
+2. Deklarasikan variabel luas dengan menggunakan rumus luas.
+luas = 0.5 * alas * tinggi
+3. Cetak hasil luas
+print(luas)
+"""
 alas = 25
 tinggi = 30
 
