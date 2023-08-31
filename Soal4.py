2b087f0573495eebdf2fa3e774a15a09e79d070b
Vynn <121730359+Vynn0@users.noreply.github.com>
2023-08-31 10:48:29 +0700

    Update Soal4.py

--- Soal4.py
+++ Soal4.py
@@ -1,16 +1,24 @@
 # 4. Tentukan algoritma dan pseudocode luas jajar genjang
-#     (panjang = 5, tinggi = 3) 
+#     (panjang = 5, tinggi = 3)
 
-# Masukkan nilai panjang jajar genjang (panjang) dan nilai tinggi jajar genjang (tinggi).
-# Hitung luas jajar genjang dengan rumus luas = panjang * tinggi.
-# Tampilkan nilai luas jajar genjang.
+"""
+Algoritma
 
+Masukkan nilai panjang jajar genjang dan nilai tinggi jajar genjang.
+Hitung luas jajar genjang dengan rumus luas = panjang * tinggi.
+Tampilkan nilai luas jajar genjang.
+
+"""
+
+"""
+PSEUDOCODE
 # MULAI
-# INPUT NILAI PANJANG
-# INPUT NILAI TINGGI
-# LUAS = PANJANG * TINGGI
-# PRINT OUTPUT LUAS
+# DEKLARASI PANJANG DENGAN NILAI 5
+# DEKLARASI TINGGI DENGAN NILAI 3
+# DEKLARASI LUAS = PANJANG * TINGGI
+# CETAK OUTPUT LUAS
 # SELESAI
+"""
 
 panjang = 5
 tinggi = 3
